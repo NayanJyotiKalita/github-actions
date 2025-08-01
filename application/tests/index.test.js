827b0e2add99effb1683ab6a03b53233bb9e1f95
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2025-08-01 22:03:22 +0530

    Create index.test.js

---
+++ application/tests/index.test.js
@@ -0,0 +1,9 @@
+const add = require("../index");
+
+test("adds 2 + 3 to equal 5", () => {
+  except(add(2, 3)).toBe(5):
+});
+
+test("add 0 +0 to equal 0", () => {
+  except(add(0, 0)).toBe(0);
+});
