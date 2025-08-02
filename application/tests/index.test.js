a39bc571034a313032c60c5c5ac9cf3725630f24
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2025-08-02 14:19:09 +0530

    Update index.test.js

--- application/tests/index.test.js
+++ application/tests/index.test.js
@@ -1,9 +1,9 @@
 const add = require("../index");
 
 test("adds 2 + 3 to equal 5", () => {
-  except(add(2, 3)).toBe(5);
+  expect(add(2, 3)).toBe(5);
 });
 
 test("adds 0 + 0 to equal 0", () => {
-  except(add(0, 0)).toBe(0);
+  expect(add(0, 0)).toBe(0);
 });
