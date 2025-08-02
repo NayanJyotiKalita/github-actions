82106d9d43b510e2e4c2fd602cfe5ecfbfe33bd9
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2025-08-02 14:17:34 +0530

    Update index.test.js

--- application/tests/index.test.js
+++ application/tests/index.test.js
@@ -4,6 +4,6 @@ test("adds 2 + 3 to equal 5", () => {
   except(add(2, 3)).toBe(5);
 });
 
-test("add 0 + 0 to equal 0", () => {
+test("adds 0 + 0 to equal 0", () => {
   except(add(0, 0)).toBe(0);
 });
